d757f593e104a3da00f4c0d0a12a9d6b2b307bd3
Bafeerk <bafeerk@mail.ru>
2021-03-14 00:06:24 +0300

    Checking functions are wrote

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -23,41 +23,62 @@ def coordinate_request(string):
             print('Введите число от 1 до 3')
             continue
 
-def check_rows(field):
+def check_rows(field, symbol):
     """
         Проверяет ряды массива на наличие трех идущих подряд 'X' или '0'
-        Принимает массив
+        Принимает массив и строку ('X' или 'Y')
         Возвращает bool
     """
-    pass
+    if field[1][1] + field[1][2] + field[1][3] == symbol * 3:
+        print('check_rows, 1')
+        return True
+    elif field[2][1] + field[2][2] + field[2][3] == symbol * 3:
+        return True
+    elif field[3][1] + field[3][2] + field[3][3] == symbol * 3:
+        return True
+    else:
+        return False
 
-def check_columns(field):
+def check_columns(field, symbol):
     """
         Проверяет столбцы массива на наличие трех идущих подряд 'X' или '0'
-        Принимает массив
+        Принимает массив и строку ('X' или 'Y')
         Возвращает bool
     """
-    pass
+    if field[1][1] + field[2][1] + field[3][1] == symbol * 3:
+        print('check_columns, 1')
+        return True
+    elif field[1][2] + field[2][2] + field[3][2] == symbol * 3:
+        return True
+    elif field[1][3] + field[2][3] + field[3][3] == symbol * 3:
+        return True
+    else:
+        return False
 
-def check_diagonals(field):
+def check_diagonals(field, symbol):
     """
         Проверяет диагонали массива на наличие трех идущих подряд 'X' или '0'
-        Принимает массив
+        Принимает массив и строку ('X' или 'Y')
         Возвращает bool
     """    
-    pass
+    if field[1][1] + field[2][2] + field[3][3] == symbol * 3:
+        return True
+    elif field[3][1] + field[2][2] + field[1][3] == symbol * 3:
+        return True
+    else:
+        return False
 
-def is_win(field):
+def is_win(field, symbol):
     """
         Проверяет поле на наличие трех идущих подряд 'X' или '0'
-        Получает массив поля
+        Получает массив поля, и строку ('X' или 'Y')
         Возвращает bool
     """
-    if check_rows(field):
+    if check_rows(field, symbol):
         return True
-    elif check_columns(field):
+    elif check_columns(field, symbol):
         return True
-    elif check_diagonals(field):
+    elif check_diagonals(field, symbol):
         return True
     else:
         return False
@@ -82,22 +103,26 @@ draw_field(field)
 while True:
     row = coordinate_request('Ряд: ')
     column = coordinate_request('Столбец: ')
+
     if field[row][column] != '-':
         draw_field(field)
         print('Поле занято')
         continue
-    if is_win(field):
+    
+    field[row][column] = 'X'
+
+    if is_win(field, 'X'):
         draw_field(field)
         print('Поздравляем, вы выиграли')
         break
-    field[row][column] = 'X'
+
     draw_field(field)
 
     computer(field)
 
-    if is_win(field):
+    if is_win(field, 'Y'):
         draw_field(field)
-        print('Поздравляем, вы выиграли')
+        print('Поздравляем, вы проиграли')
         break
     
     draw_field(field)
